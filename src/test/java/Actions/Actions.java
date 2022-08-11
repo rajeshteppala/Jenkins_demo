456c22aa1c311e53d25ccd153566a89e8b33847c
rajeshteppal <rajeshskp28@gmail.com>
2022-08-11 23:23:54 +0200

    Add takes screen shot method

--- src/test/java/Actions/Actions.java
+++ src/test/java/Actions/Actions.java
@@ -1,7 +1,14 @@
 package Actions;
 
+import io.cucumber.java.sk.Tak;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Actions {
 
@@ -21,5 +28,13 @@ public class Actions {
     public void click(WebElement element) {
         element.click();
     }
+
+    public void takeScreenShot(WebElement element, String desPath) throws IOException {
+        TakesScreenshot srcShot = ((TakesScreenshot) element);
+        File srcFile = srcShot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File(desPath);
+        FileUtils.copyFile(srcFile,destFile);
+    }
+
 
 }
